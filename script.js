5788d99a2a109efca405c4d9805a4fe7c76cc9c1
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-03 12:28:46 +1300

    hopefully alert button makes an alert

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-function myFunction()
-alert("Congratulations!\nYou got the script running.")
+function myFunction() {
+    alert("Congratulations!\nYou got the script running.")
+}
+//let alertMessage = document.getElementById("alertMessage")
+//function myFunction(type)
+
