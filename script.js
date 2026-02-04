436244e156f1dae79384cd3dcec6d99a78830fca
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-04 14:58:17 +1300

    hopefully it pushes

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function myFunction() {
-    alert("Congratulations!\nYou got the script running.")
+function getFormInput(){
+    const ALERT_FIELD = document.getElementById("myForm"); //this assigns myAlert to ALERT_FIELD as a value. I think. const is a type of vule that does not change. write them in SCREAMING_SNAKE_CASE.
+    let alertName = ALERT_FIELD.value; //this assigns the alert field to alertname.
+    document.getElementById("welcomeMessage").innerHTML = alertName;
+    console.log(alertName)
+    /*alert(alertName) // makes the alert!*/
+} // I dont know what the bracket part is used for yet.
+
+
+
+
+
+//junk codee down here, but i need it for referrence of the old code
+/*
+
+function myFunction(alert) {
+    alert("Congratulations!\nYou got the script running.");
+    console.log(alertmessage)
 }
+
 console.log("working?")
 //let alertMessage = document.getElementById("alertMessage")
-//function myFunction(type)
+//function myFunction(type)*/
+
+
 
