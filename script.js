e476899546c488f9620a3030161f6a0c32406e79
Nicholas Phang <23058np@hvhs.school.nz>
2026-02-04 14:54:03 +1300

    Extension done. learned lots.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function myFunction() {
     alert("Congratulations!\nYou got the script running.")
 }
+console.log("working?")
 //let alertMessage = document.getElementById("alertMessage")
 //function myFunction(type)
 
